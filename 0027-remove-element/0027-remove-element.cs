dd08b6177c24c33544365778d32a6c36e0b56473
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2023-10-13 20:22:18 -0500

    Time: 138 ms (13.12%), Space: 42.6 MB (86.72%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int x = 0;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i] != val){
+                nums[x] = nums[i];
+                x++;
+            }
+        }
+        return x;
+    }
+}
