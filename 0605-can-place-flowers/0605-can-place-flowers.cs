71ad8cfe3349c0771dcc193d4d882b8be6930e54
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2023-10-22 19:30:13 -0500

    Time: 108 ms (21.48%), Space: 45.8 MB (65.86%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+        if(n==0) return true;
+     for(int i=0;i<flowerbed.Length;i++){
+         if(flowerbed[i]==0 && (i==0 || flowerbed[i-1]==0) && (i==flowerbed.Length-1 || flowerbed[i+1]==0)){
+             --n;
+             if(n==0) return true;
+             flowerbed[i]=1;
+         }
+     }
+        return false;
+    }
+}
