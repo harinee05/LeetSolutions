a138e67ce1e2cfcc98966b4355194f5cde8fbd93
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-01-05 23:10:13 -0500

    Time: 68 ms (84.34%), Space: 39.8 MB (16.04%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int NumIdenticalPairs(int[] nums) {
+        int c=0;
+       for(int i=0;i<nums.Length;i++){
+           for(int j=i+1;j<nums.Length;j++){
+               if(nums[i] - nums[j]==0){
+                   c++;
+               }
+           }
+       }
+        return c;
+        
+    }
+}
