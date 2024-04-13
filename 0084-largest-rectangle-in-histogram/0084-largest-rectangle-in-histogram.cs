52a42d2ba42f1443ac762f2fcf52ac645ae6daad
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-04-13 12:00:19 -0400

    Time: 293 ms (93.79%), Space: 58.7 MB (23.50%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cs
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cs
@@ -1,28 +1,41 @@
 public class Solution {
-    public int LargestRectangleArea(int[] heights) {
-        Stack<int> stack = new Stack<int>();
-    int maxArea = 0;
+    public int LargestRectangleArea(int[] heights) 
+        //Stack<int> stack = new Stack<int>();
+   // int maxArea = 0;
 
-    // First pass: Calculate the area of rectangles formed by the bars
-    for (int index = 0; index < heights.Length; index++) {
-        while (stack.Count > 0 && heights[stack.Peek()] > heights[index]) {
+    //for(int i=0;i<heights.Length;i++){
+             
+       // while(stack.Count >0 && heights[stack.Peek()]!=height[i]){
+            //if(stack.Peek() > heights[i]){
+              //  int t = stack.Pop();
+               // int area = 
+          //  }
+               //stack.Push(heights[i]);
+       
+    {
+        
+     Stack<int> stack = new Stack<int>();
+    int maxArea = 0;
+    int index = 0;
+
+    while (index < heights.Length) {
+        if (stack.Count == 0 || heights[stack.Peek()] <= heights[index]) {
+            stack.Push(index++);
+        } else {
             int top = stack.Pop();
             int area = heights[top] * ((stack.Count == 0) ? index : index - stack.Peek() - 1);
             maxArea = Math.Max(maxArea, area);
         }
-        stack.Push(index);
     }
 
-    // Second pass: Handle any remaining bars in the stack
     while (stack.Count > 0) {
         int top = stack.Pop();
-        int area = heights[top] * ((stack.Count == 0) ? heights.Length : heights.Length - stack.Peek() - 1);
+        int area = heights[top] * ((stack.Count == 0) ? index : index - stack.Peek() - 1);
         maxArea = Math.Max(maxArea, area);
     }
 
     return maxArea;
-    }
 }
-               
-               
+}               
+
                
