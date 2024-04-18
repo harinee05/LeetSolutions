2ae9e5ed13cc16c680a4749840ba958e7274d641
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-04-17 21:32:32 -0400

    Time: 39 ms (5.81%), Space: 28.9 MB (56.43%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cs
+++ 0342-power-of-four/0342-power-of-four.cs
@@ -1,6 +1,10 @@
 public class Solution {
     public bool IsPowerOfFour(int n) {
-         return n > 0 && (n & (n - 1)) == 0 && (n & 0xAAAAAAAA) == 0;
+       if(n==0) return false;
+        while(n%4==0){
+            n= (n/4);
+        }
         
+        return n==1;
     }     
 }
