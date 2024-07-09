9bc7bb7bf05ba7f758d517336efb5808b2c9b968
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-07-09 00:58:51 -0400

    Time: 808 ms (12.03%), Space: 57.2 MB (9.97%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.py
+++ 1803-average-waiting-time/1803-average-waiting-time.py
@@ -4,7 +4,11 @@ class Solution:
         t, a = customers[0][0] + customers[0][1], customers[0][1]
 
         for x in customers[1:]:
-            t = max(t, x[0]) + x[1]
+            # t = max(t, x[0]) + x[1]
+            if t > x[0]:
+                t+=x[1]
+            else:
+                t=sum(x)
             a += t - x[0]
 
         return a/len(customers)
