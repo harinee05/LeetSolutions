e9aff734077b084ed7107f6eddf578d1959378ae
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-07-09 01:06:20 -0400

    Time: 441 ms (11.11%), Space: 80 MB (33.33%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cs
+++ 1803-average-waiting-time/1803-average-waiting-time.cs
@@ -3,19 +3,19 @@ public class Solution {
         
         double sum =0;
         double finalSum = 0;
-        double av =0;
+      
         for(int i=0;i<customers.Length;i++){
             if(i==0 || sum < customers[i][0])
                 {
                     sum = customers[i][1]+customers[i][0];
-                    finalSum = sum - customers[i][0];
+                   
                 }
            else{
            sum=sum+customers[i][1];  
-           finalSum= Math.Abs(sum - customers[i][0]);
+          
            }
-           av+=finalSum;
+          finalSum += sum - customers[i][0];
         }
-        return av/customers.Length;
+        return finalSum/customers.Length;
     }
 }
