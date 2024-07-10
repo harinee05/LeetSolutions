136b1181ac424089f8dd2712194b33d0c1a1b9e2
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-07-09 21:25:51 -0400

    Time: 493 ms (86.1%), Space: 135 MB (51.87%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cs
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cs
@@ -1,19 +1,17 @@
 public class Solution {
     public int[] RearrangeArray(int[] nums) {
-       var positives = nums.Where(n => n > 0).ToArray();
-        var negatives = nums.Where(n => n < 0).ToArray();
+        int n = nums.Length;
+        int[] result = new int[n];
+        int posIndex = 0, negIndex = 1;
 
-        // Combine positive and negative numbers, alternating between them
-        int[] result = positives.Zip(negatives, (p, n) => new { p, n })
-                                .SelectMany(x => new[] { x.p, x.n })
-                                .ToArray();
-
-        // Handle cases where the number of positives or negatives is odd
-        if (positives.Length % 2 != 0) {
-            result = result.Concat(positives.Skip(result.Length)).ToArray();
-        }
-        if (negatives.Length % 2 != 0) {
-            result = result.Concat(negatives.Skip(result.Length)).ToArray();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                result[posIndex] = nums[i];
+                posIndex += 2;
+            } else {
+                result[negIndex] = nums[i];
+                negIndex += 2;
+            }
         }
 
         return result;
