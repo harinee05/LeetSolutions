219d9ea9b923f2f62e4db466fb896a0fb65dc852
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-07-22 21:47:40 -0400

    Time: 110 ms (63.75%), Space: 48.7 MB (6.25%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cs
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int[] FrequencySort(int[] nums) {
-     var result = nums
+     return nums
             .GroupBy(x => x) // Group by value
             .OrderBy(group => group.Count()) // Order by frequency
-            .ThenByDescending(group => group.Key); // Then by value in descending order
-          
-        // Convert to array and return
-        return result.SelectMany(group => group).ToArray();
+            .ThenByDescending(group => group.Key) // Then by value in descending order
+            .SelectMany(group => group).ToArray(); // Flatten the groups
     }
 }
