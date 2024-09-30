8856dccbb45b7bfd3c6f9a63e53a1f02c7861d17
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-09-30 19:28:00 -0400

    Time: 26 ms (18.41%), Space: 26.7 MB (78.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cs
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -1,12 +1,19 @@
 public class Solution {
     public int ClimbStairs(int n) {
-    if(n==1) return 1;
-        int[] dp= new int[n+1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        if(n==2)
+        return 2;
+        if(n==1)
+        return 1;
+        int[] stair = new int[n+1];
+        stair[0] = 1;
+        stair[1] = 2;
+        for(int i=2;i<n;i++){
+            stair[i] = stair[i-2]+stair[i-1];
+            Console.WriteLine(stair[i]);
         }
-        return dp[n];
+        return stair[n-1];
     }
 }
+
+
+
