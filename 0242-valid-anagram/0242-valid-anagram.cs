59ae420dda97600effb07b704c434ab1210e8791
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-10-04 21:59:46 -0400

    Time: 65 ms (37.82%), Space: 41.7 MB (88.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,8 +1,27 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        s = String.Concat(s.OrderBy(c=>c));
-        t = String.Concat(t.OrderBy(c=>c));
-        return s==t;
+        if (s.Length != t.Length) return false;
 
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+        foreach (char c in s) {
+            if (charCount.ContainsKey(c)) {
+                charCount[c]++;
+            } else {
+                charCount[c] = 1;
+            }
+        }
+
+        foreach (char c in t) {
+            if (!charCount.ContainsKey(c)) {
+                return false; // Character in t not in s
+            }
+            charCount[c]--;
+            if (charCount[c] < 0) {
+                return false; // More occurrences in t than in s
+            }
+        }
+
+        return true; // All counts should be zero if they are anagrams
     }
 }
