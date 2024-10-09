088d16a58d5402dc83f19992507d889b3b71e560
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-10-09 08:34:16 -0400

    Time: 92 ms (63.13%), Space: 44.8 MB (57.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cs
+++ 0045-jump-game-ii/0045-jump-game-ii.cs
@@ -1,16 +1,14 @@
 public class Solution {
-   public int Jump(int[] nums) {
-       int jumps = 0, curEnd = 0, nextEnd = 0;
-       
-       for (int i = 0; i < nums.Length - 1; i++) {
-           nextEnd = Math.Max(nextEnd, i + nums[i]);
-           
-           if (i == curEnd) {
-               jumps++;
-               curEnd = nextEnd;
-           }
-       }
-       
-       return jumps;
-   }
+    public int Jump(int[] nums) {
+        int jumps=0;
+        int far=0,currentEnd=0;
+        for(int i=0;i<nums.Length-1;i++){
+            far = Math.Max(far,i+nums[i]);
+            if(i==currentEnd){
+                jumps++;
+                currentEnd = far;
+            }
+        }
+        return jumps;
+    }
 }
