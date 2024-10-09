c70ccf8d278152a683b93a0ed4b008259cda8b82
Harinee Purushothaman <68609457+harinee05@users.noreply.github.com>
2024-10-09 11:45:23 -0400

    Time: 54 ms (58.82%), Space: 38.5 MB (87.52%) - LeetHub

--- 0394-decode-string/0394-decode-string.cs
+++ 0394-decode-string/0394-decode-string.cs
@@ -1,9 +1,46 @@
-using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 public class Solution {
     public string DecodeString(string s) {
-        string regex = @"(?'count'\d+)\[(?'str'\w+)\]";
-        string rs = Regex.Replace(s, regex, 
-			m => string.Concat(Enumerable.Repeat(m.Groups["str"].Value, int.Parse(m.Groups["count"].Value))));
-        return (Regex.IsMatch(rs, regex)) ? DecodeString(rs) : rs;
+        Stack<int> countStack = new Stack<int>();
+        Stack<string> stringStack = new Stack<string>();
+        StringBuilder currentString = new StringBuilder();
+        int currentNumber = 0;
+
+        foreach (char c in s) {
+            if (char.IsDigit(c)) {
+                // Build the current number
+                currentNumber = currentNumber * 10 + (c - '0');
+            } else if (c == '[') {
+                // Push the current number and string onto their respective stacks
+                countStack.Push(currentNumber);
+                stringStack.Push(currentString.ToString());
+                // Reset for the next segment
+                currentNumber = 0;
+                currentString.Clear();
+            } else if (c == ']') {
+                // Pop from the stacks and build the decoded string
+                string prevString = stringStack.Pop();
+                int repeatCount = countStack.Pop();
+                currentString = new StringBuilder(prevString + currentString.ToString().Repeat(repeatCount));
+            } else {
+                // Append the current character to the current string
+                currentString.Append(c);
+            }
+        }
+
+        return currentString.ToString();
+    }
+}
+
+public static class StringExtensions {
+    public static string Repeat(this string str, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            result.Append(str);
+        }
+        return result.ToString();
     }
 }
